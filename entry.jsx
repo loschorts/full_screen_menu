de9b2f961241efa6262aa49ce1ef280a274c494e
Ken Tran <loschorts@gmail.com>
2017-02-14 10:45:56 -0800

    create root component and entry file

--- entry.jsx
+++ entry.jsx
@@ -1,1 +1,14 @@
-alert("hi");
+import React from 'react';
+import { render } from 'react-dom';
+
+const Root = () => {
+	return (
+		<div>
+			React
+		</div>
+	);
+}
+
+document.addEventListener("DOMContentLoaded", ()=>{
+	render(<Root/>, document.querySelector("#root"));
+});
