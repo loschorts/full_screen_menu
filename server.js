b668df9b1c0be5871bad8cd9386fa44a66e4b5b8
Ken Tran <loschorts@gmail.com>
2017-02-15 16:58:58 -0800

    add static server

--- server.js
+++ server.js
@@ -1,10 +1,5 @@
-var express = require('express')
-var app = express()
-
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
-
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+var connect = require('connect');
+var serveStatic = require('serve-static');
+connect().use(serveStatic(__dirname)).listen(8080, function(){
+    console.log('Server running on 8080...');
+});
